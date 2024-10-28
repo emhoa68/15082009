7e01be592bcc2d6079c975df3326ab4676085e7b
emhoa68 <luongquanghoa2009@gmail.com>
2024-10-28 12:12:29 +0700

    locket3.js

--- locket3.js
+++ locket3.js
@@ -5,9 +5,9 @@
 [Script]
 # ~ By emhoa68
 # ～ Egern transfer to Shadowrocket & Surge & LanceX
-revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/emhoa68/emhoa68/master/locket1.js, requires-body=true, max-size=-1, timeout=60
+revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/emhoa68/15082009/master/locket1.js, requires-body=true, max-size=-1, timeout=60
 
-deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/emhoa68/emhoa68/master/locket2.js, timeout=60
+deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/emhoa68/15082009/master/locket2.js, timeout=60
 
 [MITM]
 hostname = %APPEND% api.revenuecat.com
